f3ba54b009c0a1d827f30b725a4240380b828b1c
Yago Santos <>
2023-08-05 11:32:44 +0100

    create api project

---
+++ BACK/src/services/B.API/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace B.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
+
